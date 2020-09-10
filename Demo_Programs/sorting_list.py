1b8738de9861d8e9a55aa392a6864d5a80711ff2
Noah Ho <noah.ho@GCCYJPVQJ1WK.local>
2020-09-10 09:25:31 -0400

    first push

---
+++ Demo_Programs/sorting_list.py
@@ -0,0 +1,26 @@
+#There is a DATA STRUCTURE with numbers in it
+#list has a length of 7 and indexs of 0 - 6
+list = [3,5,2,5,2,6,4,9]
+print(list) #print list
+list.sort() #sort sorts the list
+print(list) #prints sorted list
+'''
+How many functions? 
+	- 2
+What are the names? 
+	- print,sort (look for brackets after the word)
+What is the difference between the 2? 
+	- print is static method meaning its called by itself
+	- sort is an instance method meaning its called with an instane
+'''
+str = "Noah"
+str.upper()
+print(str)
+
+#TAKEAWAY: 
+#When talking about functions always ask 
+# - what is the name of the function?
+# - what does the function take - parameters (think of type(in the brackets))
+# - Is the function called with implied pbject (uses . notation)
+# - what does the function return
+
