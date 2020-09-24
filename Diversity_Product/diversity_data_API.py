5bf6a8a82629e9309cabab66d09195aef58c7292
Noah Ho <noah.ho@GCCYJPVQJ1WK.local>
2020-09-24 11:19:48 -0400

    Diversity

---
+++ Diversity_Product/diversity_data_API.py
@@ -0,0 +1,14 @@
+import requests
+import json
+from pprint import pprint
+
+fname = input("What is the first name: ")
+lname = input("What is the last name:")
+
+#resp = requests.get("https://api.diversitydata.io/?fullname="+fname+"%20"+lname)
+
+resp = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+data = resp.json()
+
+pprint(data)
